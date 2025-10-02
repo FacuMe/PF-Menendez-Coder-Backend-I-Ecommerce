ea9fbb6aee68a8b7c7e153d50319186b3f3825cc
FacuM <facmenendez@gmail.com>
2025-10-02 01:24:16 -0300

    implementacion productos

--- src/dao/ProductsManager.js
+++ src/dao/ProductsManager.js
@@ -1,26 +1,46 @@
 const fs=require("fs")
 
-
 class ProductsManager{
-
 
     static path="../data/products.json"
 
-    static async addProduct(title, description, code, price, status, stock, category, thumbnails){
-        // validaciones
+    static async getProducts(){
+        if(fs.existsSync(this.path)){
+            let products=JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
+            products=products.map(u=>{
+                return {
+                    ...u, 
+                    title: u.title.toUpperCase()
+                }
+            })
+            return products
+        }else{
+            return []
+        }
+    }
+
+    static async getProductById(id) {
+        const products = await this.getProducts();
+        const product = products.find((p) => p.id == id);
+        return product;
+    }
+
+    static async addProduct(product){
+        const { title, description, code, price, status, stock, category, thumbnails } = product;
+
         if(!title || !description || !code || !price || !status || !stock || !category || !thumbnails){
-            throw new Error(`title | description | code | price | status | stock | category | thumbnails son requeridos`)
+            throw new Error(`title | description | code | price | status | stock | category | thumbnails are required`)
         }
 
         let products = await this.getProducts()
         let existe = products.find(u=>u.code.toLowerCase()==code.toLowerCase())
         if(existe){
-            throw new Error(`Ya existe un producto con el código ${code}: es ${existe.description} - Cod. ${existe.code}`)  
+            throw new Error(`A product with code ${code} already exists: ${existe.description}`)  
         }
 
-        let id=1
-        if(products.length>0){
-            id=Math.max(...products.map(d=>d.id))+1
+        let id = 1
+        if(products.length > 0){
+            id = Math.max(...products.map(d=>d.id)) + 1
         }
 
         let newProduct={
@@ -42,21 +62,34 @@ class ProductsManager{
         return newProduct
     }
 
-    static async getProducts(){
-        if(fs.existsSync(this.path)){
-            let products=JSON.parse(await fs.promises.readFile(this.path, "utf-8"))
-            products=products.map(u=>{
-                return {
-                    ...u, 
-                    title: u.title.toUpperCase()
-                }
-            })
-            return products
-        }else{
-            return []
-        }
+    static async updateProduct(id, updates) {
+        let products = await this.getProducts();
+
+        products = products.map(p => {
+            if (p.id == id) {
+            updates.id = p.id; 
+            return updates;     
+            }
+            return p;
+        });
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
+        return updates;
     }
 
+    static async deleteProduct(id) {
+        let products = await this.getProducts();
+        let initialLength = products.length;
+
+        products = products.filter(p => p.id != id); 
+
+        if (products.length === initialLength) {
+            return false;
+        }
+
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, 5));
+        return true;
+    }
 
 }
 
